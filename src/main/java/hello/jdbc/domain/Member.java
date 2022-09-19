037d0278c2c4231151d05648eb71f736654a0e83
KyungTae Bae <kyungtae@marvrus.com>
2022-09-19 11:57:51 +0900

    트랜잭션 적용

--- src/main/java/hello/jdbc/domain/Member.java
+++ src/main/java/hello/jdbc/domain/Member.java
@@ -4,10 +4,12 @@ import lombok.Data;
 
 @Data
 public class Member {
+
     private String memberId;
     private int money;
 
     public Member() {
+
     }
 
     public Member(String memberId, int money) {
